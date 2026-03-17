69d0bb242fe36851cefb83253f1633e60300c6bf
aklil <akliluyirgalem@gmail.com>
2026-03-17 22:22:39 +0300

    add constraint program stake test

---
+++ tests/constraint-program-stake/tests/constraint-program-stake.rs
@@ -0,0 +1,66 @@
+use {
+    lotic::pinocchio::{error::ProgramError, Address},
+    mollusk_svm::{result::Check, Mollusk},
+    solana_sdk::{
+        account,
+        instruction::{AccountMeta, Instruction},
+    },
+};
+
+#[test]
+fn test_constraint_program_tokenkeg_success() {
+    let program_id = Address::from_str_const("2JF8AjwkmCz6brkAkJf8NEEKhg89a8KrTuDZiZ5cVdS2");
+    let mollusk = Mollusk::new(
+        &program_id,
+        "../../target/deploy/test_constraint_program_stake",
+    );
+
+    let user = Address::new_unique();
+    let stake_account = Address::from_str_const("Stake11111111111111111111111111111111111111");
+    let accounts = vec![
+        (user, account::Account::default()),
+        (stake_account, account::Account::default()),
+    ];
+
+    let instruction = Instruction {
+        program_id,
+        accounts: vec![
+            AccountMeta::new(user, true),
+            AccountMeta::new(stake_account, false),
+        ],
+        data: vec![0],
+    };
+
+    mollusk.process_and_validate_instruction(&instruction, &accounts, &[Check::success()]);
+}
+
+#[test]
+fn test_constraint_program_tokenkeg_failure() {
+    let program_id = Address::from_str_const("2JF8AjwkmCz6brkAkJf8NEEKhg89a8KrTuDZiZ5cVdS2");
+    let mollusk = Mollusk::new(
+        &program_id,
+        "../../target/deploy/test_constraint_program_stake",
+    );
+
+    let user = Address::new_unique();
+    let stake_account = Address::new_unique();
+    let accounts = vec![
+        (user, account::Account::default()),
+        (stake_account, account::Account::default()),
+    ];
+
+    let instruction = Instruction {
+        program_id,
+        accounts: vec![
+            AccountMeta::new(user, true),
+            AccountMeta::new(stake_account, false),
+        ],
+        data: vec![0],
+    };
+
+    mollusk.process_and_validate_instruction(
+        &instruction,
+        &accounts,
+        &[Check::err(ProgramError::IncorrectProgramId)],
+    );
+}
